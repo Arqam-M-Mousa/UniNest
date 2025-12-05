26a29c34460f180a002d9d127f381739673bf31b
Arqam-M-Mousa <arqam.mousa@gmail.com>
2025-12-05 16:03:31 +0200

    Add message model.

---
+++ Backend/models/Message.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Message = sequelize.define(
+  "Message",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    attachmentsJson: {
+      type: DataTypes.JSON,
+      allowNull: true,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    conversationId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "conversations",
+        key: "id",
+      },
+    },
+    senderId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+  },
+  {
+    tableName: "messages",
+    timestamps: true,
+    underscored: true,
+  }
+);
+
+module.exports = Message;

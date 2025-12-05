0da296479874215d79ba40ab7c0d42fd33a2c6f9
Arqam-M-Mousa <arqam.mousa@gmail.com>
2025-12-05 16:02:53 +0200

    Add conversation model.

---
+++ Backend/models/Conversation.js
@@ -0,0 +1,64 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Conversation = sequelize.define(
+  "Conversation",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    lastMessageAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    studentLastReadAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    landlordLastReadAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    studentId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+    landlordId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+    propertyId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "property_listings",
+        key: "id",
+      },
+    },
+  },
+  {
+    tableName: "conversations",
+    timestamps: true,
+    underscored: true,
+  }
+);
+
+module.exports = Conversation;

0f7ed45760685ffd3ef9c522f72132b935afaf1b
Arqam-M-Mousa <arqam.mousa@gmail.com>
2025-12-05 16:02:47 +0200

    Add university model.

---
+++ Backend/models/University.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const University = sequelize.define(
+  "University",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    domain: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "universities",
+    timestamps: true,
+    underscored: true,
+  }
+);
+
+module.exports = University;

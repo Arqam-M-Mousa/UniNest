0866e6afc4fb6dfc32b119365736698505dcbfab
Arqam-M-Mousa <arqam.mousa@gmail.com>
2025-12-07 23:02:56 +0200

    Add routes for sending and verifying verification codes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const authService = require("../services/authService");
 const router = express.Router();
 
+router.post("/send-verification-code", authService.sendVerificationCode);
+router.post("/verify-code", authService.verifyCode);
 router.post("/signup", authService.signup);
 router.post("/signin", authService.signin);
 

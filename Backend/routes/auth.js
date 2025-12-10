b81596874703b0096aa078a4b10d23d5a73c86b0
Arqam-M-Mousa <arqam.mousa@gmail.com>
2025-12-10 20:57:35 +0200

    Add forgot password routes.

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -6,5 +6,7 @@ router.post("/send-verification-code", authService.sendVerificationCode);
 router.post("/verify-code", authService.verifyCode);
 router.post("/signup", authService.signup);
 router.post("/signin", authService.signin);
+router.post("/forgot-password", authService.forgotPassword);
+router.post("/reset-password", authService.resetPassword);
 
 module.exports = router;
